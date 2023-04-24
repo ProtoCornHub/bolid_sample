79b35b232905ed806491436292c9d0552659e45f
Rakhimyan <rahimtoy@gmail.com>
2023-04-24 23:02:54 +0300

    SensorDetails

--- lib/src/features/sensor/presentation/view/sensor_details_screen.dart
+++ lib/src/features/sensor/presentation/view/sensor_details_screen.dart
@@ -14,7 +14,56 @@ class SensorDetailsScreen extends StatelessWidget {
         title: sensor.name,
         automaticallyImplyLeading: true,
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
+        child: Column(
+          children: [
+            _DetailCard(
+              title: 'Status',
+              label: sensor.status.toString(),
+            ),
+            const SizedBox(height: 20),
+            _DetailCard(
+              title: 'Temperature',
+              label: sensor.temperature.toString(),
+            ),
+            const SizedBox(height: 20),
+            _DetailCard(
+              title: 'Humidity',
+              label: sensor.humidity.toString(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DetailCard extends StatelessWidget {
+  const _DetailCard({
+    super.key,
+    required this.title,
+    required this.label,
+  });
+  final String title;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          '$title:',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: Text(
+            label,
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+        ),
+      ],
     );
   }
 }
